4f491f6088a8c9cd757b7607d5d0c8f4843c5c08
HappyNoose <78141897+MrZahourek@users.noreply.github.com>
2025-06-15 14:30:21 +0200

    new cords

--- js/res/assetData.js
+++ js/res/assetData.js
@@ -9,3 +9,14 @@ export const ROOM_BACKGROUNDS = new Map([
     ["kid bedroom", "images/kid bedroom.jpg"],
     ["death", "images/death.jpg"]
 ]);
+
+export const CLOSET_PATHS = {
+    1: { open: "images/closet 1 open.png", closed: "images/closet 1 closed.png" },
+    2: { open: "images/closet 1 open.png", closed: "images/closet 1 closed.png" },
+    3: { open: "images/closet 2 open.png", closed: "images/closet 2 closed.png" },
+    4: { open: "images/closet 2 open.png", closed: "images/closet 2 closed.png" },
+    5: { open: "images/closet 1 open.png", closed: "images/closet 1 closed.png" },
+    6: { open: "images/closet 2 open.png", closed: "images/closet 2 closed.png" },
+    7: { open: "images/closet 1 open.png", closed: "images/closet 1 closed.png" },
+    8: { open: "images/closet 2 open.png", closed: "images/closet 2 closed.png" }
+}
